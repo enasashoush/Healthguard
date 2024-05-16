e45e286762a85ce2a6b4072c831456e1c8810759
alaa987 <alaamahmoud0001@gmail.com>
2024-05-16 19:12:12 +0300

    cart doneeeeeeeeeeeee

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,23 +10,25 @@ export default function Cart() {
     const { cartProduct, clearAllProduct, updateItem, totalCartProduct, numOfCartItem, deleteItem, setCartProduct } = useContext(CartContext)
 
     //increment element from  cart 
-    async function incremunt(itemId,newQuantity) {
-        const res = await updateItem(itemId,newQuantity)
+    async function incremunt(itemId, currentQuantity) {
+        const count = currentQuantity + 1;
+        const res = await updateItem(itemId, count);
+        console.log("res", res)
         if (res) {
-            toast.success("Product count incremented")
+            toast.success("Product count incremented");
         } else {
-            toast.error("Error in updeted Element")
+            toast.error("Error in updating product");
         }
     }
     //decrement element from cart 
-    async function decrement(itemId,newQuantity) {
-        const newCount = newQuantity - 1;
-        if (newCount === 0) {
-            const updatedProducts = cartProduct.filter(item => item.product.itemId !== itemId);
+    async function decrement(itemId, currentQuantity) {
+        const count = currentQuantity - 1;
+        if (count === 0) {
+            const updatedProducts = cartProduct.filter(item => item.id !== itemId);
             setCartProduct(updatedProducts);
-            delElement(itemId)
+            delElement(itemId);
         } else {
-            const res = await updateItem(itemId, newQuantity);
+            const res = await updateItem(itemId, count);
             if (res) {
                 toast.success("Product count decremented");
             } else {
@@ -34,6 +36,9 @@ export default function Cart() {
             }
         }
     }
+
+
+
 
     //delete element from cart 
     async function delElement(itemId) {
@@ -49,7 +54,7 @@ export default function Cart() {
             toast.error("Error in Delete Element");
         }
     }
-    
+
     if (cartProduct === null) {
         return <>
             <div className="vh-100 d-flex justify-content-center align-items-center">
@@ -109,7 +114,7 @@ export default function Cart() {
                 {cartProduct.map(function (product, idx) {
                     return <div key={idx} className="row align-items-center border-bottom border-3 my-5 pb-3">
                         <div className="col-md-2">
-                            <img  className='w-100' src={product.picUrl}  alt={product.productName}/>
+                            <img className='w-100' src={product.picUrl} alt={product.productName} />
                             <img src="" alt="" />
 
                         </div>
@@ -120,10 +125,11 @@ export default function Cart() {
                         </div>
                         <div className="col-md-2">
                             <div className="d-flex align-items-center">
-                                <button onClick={() => { incremunt(product.id, product.newQuantity + 1) }} className='btn btn-outline-success '>+</button>
+                                <button onClick={() => incremunt(product.id, product.quanntity)} className='btn btn-outline-success'>+</button>
                                 <span className='mx-2'>{product.quanntity}</span>
-                                <button onClick={() => { decrement(product.id, product.newQuantity) }} className='btn btn-outline-success '>-</button>
+                                <button onClick={() => decrement(product.id, product.quanntity)} className='btn btn-outline-success'>-</button>
                             </div>
+
                         </div>
                     </div>
 
--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,166 +1,168 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../config";
-export const CartContext = createContext();
-export function CartContextProvider({ children }) {
+export const CartContext= createContext();
+export function CartContextProvider({children}) {
     //cart states
     const [cartProduct, setCartProduct] = useState([])
     const [totalCartProduct, setTotalCartProduct] = useState(0)
     const [numOfCartItem, setNumOfCartItem] = useState(0)
     const [cartId, setCartId] = useState(null)
-    /////cart
-    // Function to add a product to the cart
-    async function addProduct(productId) {
-        try {
-            const response = await axios.post(
-                `${API_BASE_URL}/api/Basket/add-to-basket`,
-                {
-                    "productId": productId
-                },
-                {
-                    headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
-                }
-            );
-            const data = response.data;
-            userCart();
-            return data;
-        } catch (e) {
-            console.log("Error:", e);
-        }
-    }
-
-
-    // Function to get the user's cart data
-    async function userCart() {
-        try {
-            const { data } = await axios.get(`${API_BASE_URL}/api/Basket`, {
+      /////cart
+  // Function to add a product to the cart
+  async function addProduct(productId) {
+    try {
+        const response = await axios.post(
+            `${API_BASE_URL}/api/Basket/add-to-basket`,
+            {
+                "productId":productId
+            },
+            {
                 headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
-            });
+            }
+        );
+        const data = response.data; 
+        userCart(); 
+        return data;
+    } catch (e) {
+        console.log("Error:", e);
+    }
+}
+
+
+// Function to get the user's cart data
+async function userCart() {
+    try {
+        const { data } = await axios.get(`${API_BASE_URL}/api/Basket`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
+        });
+        setNumOfCartItem(data.totalQuantity);
+        setTotalCartProduct(data.totalPrice);
+        setCartProduct(data.basket.items);
+        setCartId(data.basket.id);
+        return data;
+    } catch (e) {
+        console.log("error", e);
+    }
+}
+
+
+//delete product from cart
+async function deleteItem(itemId) {
+    try {
+        const response = await axios.delete(`${API_BASE_URL}/api/Basket/delete-from-basket/${itemId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
+        });
+
+        const data = response.data;
+        if (data) {
             setNumOfCartItem(data.totalQuantity);
             setTotalCartProduct(data.totalPrice);
             setCartProduct(data.basket.items);
-            setCartId(data.basket.id);
             return data;
-        } catch (e) {
-            console.log("error", e);
+        } else {
+            return null; 
         }
+    } catch (error) {
+        console.error("Error deleting item:", error);
     }
-
-
-    //delete product from cart
-    async function deleteItem(itemId) {
-        try {
-            const response = await axios.delete(`${API_BASE_URL}/api/Basket/delete-from-basket/${itemId}`, {
-                headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
-            });
-
-            const data = response.data;
-            if (data) {
-                setNumOfCartItem(data.totalQuantity);
-                setTotalCartProduct(data.totalPrice);
-                setCartProduct(data.basket.items);
-                return data;
-            } else {
-                return null;
-            }
-        } catch (error) {
-            console.error("Error deleting item:", error);
-        }
-    }
+}
 
     //update product from cart
-    async function updateItem(itemId, newQuantity) {
-        try {
-            const { data } = await axios.put(`${API_BASE_URL}/api/Basket/update-basket-item-quantity/${itemId}/${newQuantity}`,
+    async function updateItem(itemId,count) {
+        try{
+            const{data}=await axios.put(`${API_BASE_URL}/api/Basket/update-basket-item-quantity/${itemId}`,
                 {
-                    headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` }
-                })
+                    "count": count
+                }
+            ,{
+                headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` } 
+            })
+            console.log("2")
             setNumOfCartItem(data.totalQuantity);
             setTotalCartProduct(data.totalPrice);
             setCartProduct(data.basket.items);
-            return data;
-        }
-        catch (e) {
-            console.log("error", e)
-        }
-
+        return data;
+           } 
+           catch(e){
+            console.log("error",e)
+           }
+        
     }
     //clear all product from cart 
     async function clearAllProduct(id) {
-        try {
-            const { data } = await axios.delete(`${API_BASE_URL}/api/Basket/${id}`, {
-                "id": id
-            },
-                {
-                    headers: { token: localStorage.getItem("tkn") }
-                })
-            setNumOfCartItem(0)
-            setTotalCartProduct(0)
-            setCartProduct([])
-            return data;
-        }
-        catch (e) {
-            console.log("error", e)
-        }
-
+        try{
+            const{data}=await axios.delete(`${API_BASE_URL}/api/Basket/${id}`,
+              {
+                headers: { Authorization: `Bearer ${localStorage.getItem("tkn")}` } 
+            })
+              setNumOfCartItem(0)
+              setTotalCartProduct(0)
+              setCartProduct([])
+        return data;
+           } 
+           catch(e){
+            console.log("error",e)
+           }
+        
     }
     //wish list states
     const [wishListProduct, setWishListProduct] = useState([])
     const [wishListStatus, setWishListStatus] = useState(() => {
         const savedStatus = localStorage.getItem('wishlistStatus');
         return savedStatus ? JSON.parse(savedStatus) : {};
-    });
+      });
     //add prduct to wish list   
     async function addProductToWishList(productWishId) {
-        try {
-            const { data } = await axios.post("https://ecommerce.routemisr.com/api/v1/wishlist", {
-                "productId": productWishId
-            }, {
-                headers: { token: localStorage.getItem("tkn") }
-            })
-            userWishList()
-            return data;
+        try{
+         const{data}=await axios.post("https://ecommerce.routemisr.com/api/v1/wishlist",{
+             "productId": productWishId
+         },{
+             headers:{token:localStorage.getItem("tkn")}
+         })
+         userWishList()
+         return data;
+        } 
+        catch(e){
+         console.log("error",e)
         }
-        catch (e) {
-            console.log("error", e)
-        }
-    }
-    //api of wish list
-    async function userWishList() {
-        try {
-            const { data } = await axios.get("https://ecommerce.routemisr.com/api/v1/wishlist", {
-                headers: { token: localStorage.getItem("tkn") }
+     }
+     //api of wish list
+     async function userWishList() {
+        try{
+            const{data}=await axios.get("https://ecommerce.routemisr.com/api/v1/wishlist",{
+                headers:{token:localStorage.getItem("tkn")}
             })
             setWishListProduct(data.data)
-        }
-        catch (e) {
-            console.log("error", e)
-        }
-
+           } 
+           catch(e){
+            console.log("error",e)
+           }
+        
     }
     //delete from wish list
     async function deleteItemFromWish(productDElId) {
-        try {
-            const { data } = await axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productDElId}`, {
-                headers: { token: localStorage.getItem("tkn") }
+        try{
+            const{data}=await axios.delete(`https://ecommerce.routemisr.com/api/v1/wishlist/${productDElId}`,{
+                headers:{token:localStorage.getItem("tkn")}
             })
             setWishListProduct(data.data)
             userWishList()
-            return data;
-        }
-        catch (e) {
-            console.log("error", e)
-        }
-
+        return data;
+           } 
+           catch(e){
+            console.log("error",e)
+           }
+        
     }
-    //to can use user cart and wish list
-    useEffect(() => {
-        userCart()
-        userWishList()
-    }, [])
-    return <CartContext.Provider value={{
-        addProduct,
-        cartProduct,
+//to can use user cart and wish list
+useEffect(() => {
+    userCart()
+    userWishList()
+   }, [])
+    return<CartContext.Provider value={{
+        addProduct ,
+        cartProduct, 
         totalCartProduct,
         numOfCartItem,
         userCart,
@@ -178,8 +180,8 @@ export function CartContextProvider({ children }) {
         deleteItemFromWish,
         wishListStatus,
         setWishListStatus
-    }}>
-        {children}
+        }}>
+    {children}
     </CartContext.Provider>
-
+    
 }
