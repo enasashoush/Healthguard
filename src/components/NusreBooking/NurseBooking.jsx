de79726d792048335a58f754dab4ce45778dd12f
enasashoush <141346015+enasashoush@users.noreply.github.com>
2024-05-18 21:36:31 +0300

    slider and nurse

--- src/components/NusreBooking/NurseBooking.jsx
+++ src/components/NusreBooking/NurseBooking.jsx
@@ -26,10 +26,10 @@ export default function NurseBooking() {
         <div className='container d-flex justify-content-around m-5 gap-4'>
             <div className='doctor-cards'>
                 {/* we will add nurse name variable instead of static */}
-                <h6 className='text-muted'><span className='logo'>icon</span> We notified Nurse: enas ashraf of your booking</h6>
+                <h6 className='text-muted'><i class=" logo fa-regular fa-envelope"></i> We notified Nurse: enas ashraf of your booking</h6>
                 {/* we will add price fees variable */}
-                <h6 className='text-muted'><span className='logo'>icon</span> Examination Fees: 200 EGP</h6>
-                <h6 className='text-muted'><span className='logo'>icon</span> Booking details</h6>
+                <h6 className='text-muted my-4'><i class="logo fa-solid fa-money-bill-wave"></i> Examination Fees: 200 EGP</h6>
+                <h6 className='text-muted'><i class="logo fa-regular fa-calendar-days"></i> Booking details</h6>
                 <div className='container-fluid mt-4'>
                     <div className="d-flex justify-content-between">
                         <h6 className='text-main'>Patient name</h6>
@@ -67,43 +67,73 @@ export default function NurseBooking() {
                     <div className="col-12 border border-1 border-gray mb-2 "></div>
 
 
-                    <button className='btn btn-success '>My Resirvation</button>
+                    <div className="d-flex justify-content-center">
+                        <button className="btn btn-success" type="submit">
+                            My Resirvation
+                        </button>
+                    </div>
                 </div>
 
             </div>
 
-            <div className='doctor-cards'>
-                <h3 className='text-muted'>Notes for the nurse <span className='fs-6'>(optional)</span></h3>
+            <div className="doctor-cards">
+                <h3 className="text-muted">
+                    Notes for the nurse <span className="fs-6">(optional)</span>
+                </h3>
                 {/* change enas ashraf to be variable */}
-                <p className='text-muted' >Let Nurse: enas ashraf know a little about yourself and your health problem</p>
-                <div className='d-flex justify-content-around'>
+                <p className="text-muted">
+                    Let Nurse: enas ashraf know a little about yourself and your health problem
+                </p>
+                <div className="d-flex justify-content-around">
                     <div>
-                        <label htmlFor="">Age:</label>
-                        <input type="text" id='name' placeholder='patiant age' className="form-control  mb-3" />
+                        <label htmlFor="" className="mb-2">
+                            Age:
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="patient age"
+                            className="form-control mb-3"
+                        />
                     </div>
                     <div>
-                        <label htmlFor="">Gender:</label> <br />
+                        <label htmlFor="" className="mb-2">
+                            Gender:
+                        </label> <br />
                         <input type="radio" name="gender" value="M" />
-                        <label className='text-muted' > male</label>
+                        <label className="text-muted me-4   ">male</label>
                         <input type="radio" name="gender" value="f" />
-                        <label className='text-muted' > female</label>
+                        <label className="text-muted">female</label>
                     </div>
                 </div>
 
-                <div>
-                    <label htmlFor="">Symptoms:</label>
-                    <input type="text" id='name' placeholder='write about his health situation' className="form-control mb-3" />
+                <div className='ms-5' >
+                    <label htmlFor="" >Symptoms:</label>
+                    <br />
+                    <textarea
+                        type="text"
+                        id="name"
+                        placeholder="write about his health situation"
+                        className="form-control mb-3 "
+                    />
                 </div>
-{/* 
-                disabled={formikObj.isValid === false || formikObj.dirty === false} */}
-                {/* {isLoading ? <FallingLines
-                    color="#072E33"
-                    width="50"
-                    visible={true}
-                    ariaLabel='falling-lines-loading'
-                /> : 'send to the nurse'} */}
-                <button className='btn btn-success'  type='submit'> send to the nurse</button>
 
+                {/* disabled={formikObj.isValid === false || formikObj.dirty === false} */}
+                {/* {isLoading ? (
+    <FallingLines
+      color="#072E33"
+      width="50"
+      visible={true}
+      ariaLabel="falling-lines-loading"
+    />
+  ) : (
+    'send to the nurse'
+  )} */}
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-success" type="submit">
+                        send to the nurse
+                    </button>
+                </div>
             </div>
         </div>
 
